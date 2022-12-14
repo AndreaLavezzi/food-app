8b668149ec6dc930d230e3466e4883fc1947ba85
AndreaLavezzi <andrea.lavezzi@iisviolamarchesini.edu.it>
2022-12-14 15:53:33 +0100

    Prove login

--- main/lib/logintry.dart
+++ main/lib/logintry.dart
@@ -1,8 +1,11 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:food_app/pages/category.dart';
+import 'package:dio/dio.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -12,6 +15,9 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   //String _email;
   //String _password;
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext conterxt) {
     return Scaffold(
@@ -44,23 +50,43 @@ class _LoginPageState extends State<LoginPage> {
                     child: Column(
                   children: <Widget>[
                     TextFormField(
-                      decoration: InputDecoration(labelText: 'e-mail'),
+                      decoration: InputDecoration(labelText: 'E-mail'),
+                      controller: emailController,
                     ),
                     TextFormField(
-                      decoration: InputDecoration(labelText: 'password'),
+                      decoration: InputDecoration(labelText: 'Password'),
+                      controller: passwordController,
                       obscureText: true,
                     ),
-                    ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => HomePage()));
-                        },
-                        child: Text('Accedi'))
+                    Container(
+                        margin: EdgeInsets.only(top: 30.0),
+                        child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => HomePage()));
+                            },
+                            child: Text('Accedi')))
                   ],
                 ))
               ],
             )));
+  }
+}
+
+Future<String> postLogin(email, password) async {
+  try {
+    var dio = Dio();
+    var url = "http://localhost/food-api/API/user/login.php";
+    print(email + " " + password);
+    var response =
+        await dio.post(url, data: {'email': email, 'password': password});
+    var responseBody = response.data.toString();
+    var decoded = jsonDecode(responseBody);
+    return decoded;
+  } catch (e) {
+    print(e);
+    return "";
   }
 }
